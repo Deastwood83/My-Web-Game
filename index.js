b23ae140cd1d75c75a947386477b9d41b22defc5
unknown <danieleastwood4@gmail.com>
2022-03-02 18:52:31 -0500

    adding player two

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = 1800
-canvas.height = 900
+canvas.width = 1885
+canvas.height = 950
 
-class Player1 {
-    constructor() {
+class Player {
+    constructor(xStart, yStart, img) {
+        //add x and y position for where the player starts
         this.position = {
-            x: 200,
-            y: 200
+            x: xStart,
+            y: yStart
         }
+        //add a velocity for when the player begins moving
         this.velocity = {
             x:0,
             y:0
         }
         
         const image = new Image()
-        image.src = './assets/player 1 ship.png'
+        image.src = img
     
         this.image = image
-        this.width = 250
-        this.height = 250
+        this.width = 200
+        this.height = 200
     }
 
-
+    //draw is a canvas function used to add elements to the canvas
     draw() {
         c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
     }
 
+    //update function used to update x and y position of character when moving
     update(){
         if (this.image) {
         this.draw()
@@ -37,17 +40,20 @@ class Player1 {
     }
 }
 
-const firstPlayer = new Player1()
-firstPlayer.draw()
+const firstPlayer = new Player(150, 350, './assets/player 1 ship.png')
+const secondPlayer = new Player(1500, 350, './assets/player 2 ship.png')
 
 //create animate function to load image fast 
 function animate(){
     //fillStyle and fillRect allow modification of canvas css
     c.fillStyle= 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+    //requestAnimationFrame to repeatidly call the animate function
     window.requestAnimationFrame(animate)
     firstPlayer.draw()
     firstPlayer.update()
+    secondPlayer.draw()
+    secondPlayer.update()
 }
 animate()
 
@@ -82,6 +88,40 @@ addEventListener('keyup', ({key}) =>{
             break
         case 'w':
             firstPlayer.velocity.y = 0
+            break
+    }
+})
+
+addEventListener('keydown', ({key}) =>{
+    switch (key) {
+        case '4':
+            secondPlayer.velocity.x = -5
+            break
+        case '6':
+            secondPlayer.velocity.x = +5
+            break
+        case '5':
+            secondPlayer.velocity.y = +5
+            break
+        case '8':
+            secondPlayer.velocity.y = -5
+            break
+    }
+})
+
+addEventListener('keyup', ({key}) =>{
+    switch (key) {
+        case '4':
+            secondPlayer.velocity.x = 0
+            break
+        case '6':
+            secondPlayer.velocity.x = 0
+            break
+        case '5':
+            secondPlayer.velocity.y = 0
+            break
+        case '8':
+            secondPlayer.velocity.y = 0
             break
     }
 })
